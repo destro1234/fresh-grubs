17886a345fcd4a09dadf559de13b7079fc1a2d2a
Elias <eliasventura23@gmail.com>
2023-04-20 18:56:08 -0400

    created Welcome component

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Welcome () {
+    return (
+        <div>
+            <h1>Welcome to FreshGrubs!</h1>
+            <h3>Sign in with your username and password</h3>
+        </div>
+    )
+}
+
+export default Welcome;
